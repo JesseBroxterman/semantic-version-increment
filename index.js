c88fee61859421ff434e84db754ae07d65598a71
Jesse Broxterman <jesse.broxterman@snco.us>
2023-07-21 12:23:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const Increment_Type = {
+    Minor: 'minor',
+    Patch: 'patch' 
+}
+
+const Labels_In_Use = {
+    Bug: 'bug',
+    Enhancement: 'enhancement'
+}
+
+try {
+    const current_Version = core.getInput('current_version');
+    const pr_labels = core.getInput('pr_labels');
+
+    //take the current version and separate it into it's components: Major, Minor, & Patch
+    let [major, minor, patch] = current_Version.split(".");
+
+    if (pr_labels.includes(Labels_In_Use.Enhancement)) {
+        minor = minor++;
+        patch = 0;
+    } else if (pr_labels.includes(Labels_In_Use.Bug)) {
+        patch = patch++;
+    } else {
+        throw new Error('There is no label on the pull request.');
+    }
+
+    let new_version = major.concat('.', minor, '.', patch);
+
+} catch (error) {
+    core.setFailed(error.message);
+}
